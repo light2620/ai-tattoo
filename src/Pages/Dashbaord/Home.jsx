eda7c75ff024a7d0683bf0f21f0a660b82b6d301
shivam <shivamnegi896@gmail.com>
2025-05-21 16:47:17 +0530

    m

--- src/Pages/Dashbaord/Home.jsx
+++ src/Pages/Dashbaord/Home.jsx
@@ -10,7 +10,7 @@ import { FaDownload } from 'react-icons/fa';
 const Home = () => {
   const [input, setInput] = useState('');
   const [loading, setLoading] = useState(false);
-  const [imageUrl, setImageUrl] = useState("")
+  const [imageUrl, setImageUrl] = useState("https://firebasestorage.googleapis.com/v0/b/tattoo-shop-printing-dev.firebasestorage.app/o/tattoos%2F600703dd-832c-4c22-9fd5-7af3d55a20c7.png?alt=media&token=1f6593b1-e5f6-4dab-b932-dd74cafe80de")
   const [quality, setQuality] = useState('simple');
   const { post } = useApi();
   const dispatch = useDispatch();
