ed7ca867ae5e25e155c23ee33b0b66704dd60b59
shivam <shivamnegi896@gmail.com>
2025-05-21 18:52:36 +0530

    fixed forgot password

--- src/Pages/Auth/ForgotPassword.jsx
+++ src/Pages/Auth/ForgotPassword.jsx
@@ -5,21 +5,22 @@ import { InputText } from 'primereact/inputtext';
 import { Link } from 'react-router-dom'; // for routing
 import { useUser } from '../../Context/userContext';
 const ForgotPassword = () => {
-   const [ credentials, setCredentials ] = useState({
-          email: ""
-      })
-  const { resetPassword,loading} = useUser(); 
-  
+  const [credentials, setCredentials] = useState({
+    email: ""
+  })
+  const { resetPassword, loading } = useUser();
+
   function handleLogin(e) {
     e.preventDefault();
-    resetPassword(credentials,setCredentials);
+    resetPassword(credentials, setCredentials);
   }
 
   return (
 
-      <form onSubmit={handleLogin} className="auth-panel">
-        <h1 className="forgot-password-heading">Forgot Password</h1>
-        <div className="auth-input">
+    <form onSubmit={handleLogin} className="auth-panel">
+      <h1 className="forgot-password-heading">Forgot Password</h1>
+      <div className="auth-input-container">
+        <div className="auth-inputs">
           <label htmlFor="login-email">Email</label>
           <input
             className="auth-input"
@@ -31,14 +32,16 @@ const ForgotPassword = () => {
             required
           />
         </div>
-        <div className="auth-links">
-            <Link className="auth-link" to="/signin">Back to Sign In</Link>
-        </div>
-        <button type="submit" disabled={loading} className="auth-btn">
-                  {loading && <i className="pi pi-spin pi-spinner"></i>}
-                  <span>Submit</span>
-                </button>
-      </form>
+      </div>
+
+      <div className="auth-links">
+        <Link className="auth-link" to="/signin">Back to Sign In</Link>
+      </div>
+      <button type="submit" disabled={loading} className="auth-btn">
+        {loading && <i className="pi pi-spin pi-spinner"></i>}
+        <span>Submit</span>
+      </button>
+    </form>
 
   );
 };
