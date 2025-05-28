fe3ee94e449e27dd7b7e2edb50375d81f77f2055
shivam <shivamnegi896@gmail.com>
2025-05-28 23:29:55 +0530

    added new logo

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,5 +1,5 @@
 
-import logo from '../Assets/Logo.png';
+import logo from '../Assets/logo2.jpeg';
 import { useNavigate } from 'react-router-dom';
 export default function Logo() {
     const navigate = useNavigate();
