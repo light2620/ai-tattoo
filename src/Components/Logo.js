027ec77200340b1c4d253c09b505c727617988fb
shivam <shivamnegi896@gmail.com>
2025-05-29 00:14:00 +0530

    logo updated

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,5 +1,5 @@
 
-import logo from '../Assets/logo2.jpeg';
+import logo from '../Assets/logo2.png';
 import { useNavigate } from 'react-router-dom';
 export default function Logo() {
     const navigate = useNavigate();
